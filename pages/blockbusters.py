e89263737febe4fdd6b3b01f6d354f6d4e78e598
Shirley23H <shirleyhuang0428@gmail.com>
2024-06-01 11:21:44 +0200

    Update blockbusters.py

--- pages/blockbusters.py
+++ pages/blockbusters.py
@@ -13,11 +13,16 @@ import requests
 from sklearn.decomposition import PCA
 import random
 
-# Load pickle files
-df_bl = pd.read_pickle("./pages/df_bl.pkl.gz")
-df_bl_actor = pd.read_pickle("./pages/df_bl_actor.pkl.gz")
-tmdb_df_1 = pd.read_pickle("./pages/tmdb_df_1.pkl.gz")
-df_bl_genres = pd.read_pickle("./pages/df_bl_genres.pkl.gz")
+# Load pickle files from GitHub
+df_bl_url = "https://raw.githubusercontent.com/Shirley23H/Movies-Recommandation-With-Machine-Learning/main/datasets/df_bl.pkl.gz"
+df_bl_actor_url = "https://raw.githubusercontent.com/Shirley23H/Movies-Recommandation-With-Machine-Learning/main/datasets/df_bl_actor.pkl.gz"
+tmdb_df_1_url = "https://raw.githubusercontent.com/Shirley23H/Movies-Recommandation-With-Machine-Learning/main/datasets/tmdb_df_1.pkl.gz"
+df_bl_genres_url = "https://raw.githubusercontent.com/Shirley23H/Movies-Recommandation-With-Machine-Learning/main/datasets/df_bl_genres.pkl.gz"
+
+df_bl = pd.read_pickle(df_bl_url)
+df_bl_actor = pd.read_pickle(df_bl_actor_url)
+tmdb_df_1 = pd.read_pickle(tmdb_df_1_url)
+df_bl_genres = pd.read_pickle(df_bl_genres_url)
 
 
 # Extract the list of actors
